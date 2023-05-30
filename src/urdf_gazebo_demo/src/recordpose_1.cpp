88c996ff61bae96ed889d67aa1aad4843194561b
LPGUAIA <2158494709@qq.com>
2023-05-31 03:57:40 +0800

    存一下，写入有bug

--- src/urdf_gazebo_demo/src/recordpose_1.cpp
+++ src/urdf_gazebo_demo/src/recordpose_1.cpp
@@ -13,24 +13,65 @@
 
 using namespace std;
 
-#define record_path "../pathrec/pathrecord1.txt"
+#define record_path "${HOME}/finalhw_ros/src/urdf_gazebo_demo/pathrec/pathrecord1.txt"
+
+void writefile(const char *path, string data)
+{
+    ofstream file;
+    file.open(path, ios::out | ios::app);
+    ROS_INFO("file opened");
+    file << data;
+    ROS_INFO("data written");
+    file.close();
+}
+
+string readfile(const char *path)
+{
+    ifstream file;
+    file.open(path, ios::in);
+    string data;
+    file >> data;
+    file.close();
+    return data;
+}
+void deletefile(const char *path)
+{
+    remove(path);
+}
+
+void string2char(string str, char *ch)
+{
+    int i;
+    for (i = 0; i < str.length(); i++)
+    {
+        ch[i] = str[i];
+    }
+    ch[i] = '\0';
+}
 
 void callback_path(const geometry_msgs::PoseStamped &msg)
 {
     // outfile用法同cout,存储形式 1 2 3
-    ofstream outfile;
-    outfile.setf(ios::fixed, ios::floatfield);
-    outfile.precision(2);
-    outfile.open(record_path, std::ios::app);
-    outfile << msg.pose.position.x << " " << msg.pose.position.y << endl;
+    string date;
+    date+="x=";
+    date+= to_string(msg.pose.position.x);
+    date+="; ";
+    date+="y=";
+    date+= to_string(msg.pose.position.y);
+    date+="; ";
+    date+="\n";
+    ROS_INFO("date=%s",date.c_str());
+    writefile(record_path, date);
     ROS_INFO("recorded_1");
-    outfile.close();
+
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "move_base_goal_record");
     ros::NodeHandle n;
+
+    // deletefile(record_path);
 
     ros::Subscriber rec_sub = n.subscribe("/move_base_simple/goal", 25, callback_path);
     ROS_INFO("recording_1");
