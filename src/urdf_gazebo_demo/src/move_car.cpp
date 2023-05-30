76add10ba2234d58dab16d9ca12dfb6f0cccd201
LPGUAIA <2158494709@qq.com>
2023-05-31 01:11:30 +0800

    嗯缝合

---
+++ src/urdf_gazebo_demo/src/move_car.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+int main (int argc, char **argv)
+{
+    ros::init(argc, argv, "move_");
+    ros::NodeHandle nh;
+
+    
---
+++ src/urdf_gazebo_demo/src/recordpose_2.cpp
@@ -0,0 +1,39 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <string>
+#include <iostream>
+#include <cstdio>
+#include <unistd.h>
+#include <math.h>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+void callback_path(const geometry_msgs::PoseStamped &msg)
+{
+    // outfile用法同cout,存储形式 1 2 3
+    ofstream outfile;
+    outfile.setf(ios::fixed, ios::floatfield);
+    outfile.precision(2);
+    outfile.open("/home/lpga/demo01_ws/src/urdf_gazebo_demo/pathrec/pathrecord2.txt", std::ios::app);
+    outfile << msg.pose.position.x << " " << msg.pose.position.y << endl;
+    ROS_INFO("recorded_2");
+    outfile.close();
+}
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "move_base_goal_record");
+    ros::NodeHandle n;
+
+    ros::Subscriber rec_sub = n.subscribe("/move_base_simple/goal", 25, callback_path);
+    ROS_INFO("recording_2");
+
+    ros::spin();
+    return 0;
+}
