aa40bf751028986ac206791a51b319b6ec08d199
LPGUAIA <2158494709@qq.com>
2023-06-13 03:22:05 +0800

    等会开始处理深度信息

--- src/cubemeasure/src/cubemeasure.cpp
+++ src/cubemeasure/src/cubemeasure.cpp
@@ -11,8 +11,6 @@ void imageCallback(const sensor_msgs::ImageConstPtr &msg)
     try
     {
         raw_img = cv_bridge::toCvCopy(msg, "bgr8")->image;
-        imshow("raw_img", raw_img);
-        // waitKey(0);
     }
     catch (cv_bridge::Exception &e)
     {
@@ -25,13 +23,11 @@ void depthCallback(const sensor_msgs::ImageConstPtr &msg)
     cv_bridge::CvImagePtr cv_ptr;
     try
     {
-        raw_depth = cv_bridge::toCvCopy(msg, "mono8")->image;
-        imshow("raw_depth", raw_depth);
-        // waitKey(0);
+        raw_depth = cv_bridge::toCvCopy(msg, "32FC1")->image;
     }
     catch (cv_bridge::Exception &e)
     {
-        ROS_ERROR("Could not convert from '%s' to 'mono16'.", msg->encoding.c_str());
+        ROS_ERROR("Could not convert from '%s' to '32FC1'.", msg->encoding.c_str());
     }
 }
 
@@ -39,7 +35,6 @@ void predispose(InputArray rawimg, OutputArray outimg, int border_value, int thr
 {
     Mat grayimg;
     cvtColor(rawimg, grayimg, COLOR_BGR2GRAY);
-    // imshow("grayimg", grayimg);
     Mat threshimg_max;
     Mat threshimg_min;
     Mat threshimg;
@@ -47,33 +42,41 @@ void predispose(InputArray rawimg, OutputArray outimg, int border_value, int thr
     threshold(grayimg, threshimg_min, thresh_min, 255, THRESH_BINARY);
     bitwise_and(threshimg_max, threshimg_min, threshimg);
     erode(threshimg, threshimg, Mat(), Point(-1, -1), border_value);
-    imshow("threshimg", threshimg);
+    // imshow("threshimg", threshimg);
     dilate(threshimg, outimg, Mat(), Point(-1, -1), border_value);
+    // filter2D(threshimg, outimg, -1, Mat(), Point(-1, -1), border_value);
 
-    // blur(threshimg, threshimg, Size(blur_size, blur_size));
-    // threshold(threshimg, outimg, 127, 255, THRESH_BINARY);
+    // imshow("outimg", outimg);
+}
 
-    imshow("outimg", outimg);
+float p2l_distance(Point2f point1, Point2f point2, Point2f point)
+{
+    Point2f vec1 = point1 - point2;
+    Point2f vec2 = point - point2;
+    float cross = vec1.x * vec2.y - vec1.y * vec2.x;
+    float length = cv::norm(vec1);
+    float distance = cross / length;
+    return distance;
+}
+
+void findmaxsizeContours(vector<vector<Point>> contours, vector<Point> &maxsizeContours)
+{
+    int maxsize = 0;
+    for (int i = 0; i < contours.size(); i++)
+    {
+        if (contours[i].size() > maxsize)
+        {
+            maxsize = contours[i].size();
+            maxsizeContours = contours[i];
+        }
+    }
 }
 
 vector<Point> contoursfilter(vector<vector<Point>> contours)
 {
 
     vector<Point> simplifiedcontours;
-    for (int i = 0; i < contours.size(); i++)
-    {
-        if (i == 0)
-        {
-            simplifiedcontours = contours[i];
-        }
-        else
-        {
-            if (contours[i].size() > simplifiedcontours.size())
-            {
-                simplifiedcontours = contours[i];
-            }
-        }
-    }
+    findmaxsizeContours(contours, simplifiedcontours);
     approxPolyDP(simplifiedcontours, simplifiedcontours, 5, true);
     vector<int> hull;
     convexHull(simplifiedcontours, hull);
@@ -88,172 +91,266 @@ vector<Point> contoursfilter(vector<vector<Point>> contours)
         center_point += hull_points[i];
     }
     center_point *= (1. / hull_points.size());
+
+    std::sort(hull_points.begin(), hull_points.end(), [center_point](cv::Point a, cv::Point b)
+              { return std::atan2(a.y - center_point.y, a.x - center_point.x) < std::atan2(b.y - center_point.y, b.x - center_point.x); });
+
     simplifiedcontours = hull_points;
-
-    // int minidelta_xy = 30;
-
-    // for (int i = 0; i < simplifiedcontours.size(); i++)
-    // {
-    //     if (i == 0)
-    //     {
-    //         if ((abs(simplifiedcontours[i].x - simplifiedcontours[simplifiedcontours.size() - 1].x) < minidelta_xy) && (abs(simplifiedcontours[i].y - simplifiedcontours[simplifiedcontours.size() - 1].y) < minidelta_xy))
-    //         {
-    //             if (simplifiedcontours[i].y < center_point.y)
-    //             {
-    //                 if (simplifiedcontours[i].y < simplifiedcontours[i - 1].y)
-    //                 {
-    //                     simplifiedcontours[i - 1].x = simplifiedcontours[i].x;
-    //                     simplifiedcontours[i - 1].y = simplifiedcontours[i].y;
-    //                 }
-    //             }
-    //             else
-    //             {
-    //                 if (simplifiedcontours[i].y > simplifiedcontours[i - 1].y)
-    //                 {
-    //                     simplifiedcontours[i - 1].x = simplifiedcontours[i].x;
-    //                     simplifiedcontours[i - 1].y = simplifiedcontours[i].y;
-    //                 }
-    //             }
-    //             simplifiedcontours.erase(simplifiedcontours.begin() + i);
-    //         }
-    //     }
-    //     else
-    //     {
-    //         if ((abs(simplifiedcontours[i].x - simplifiedcontours[i - 1].x) < minidelta_xy) && (abs(simplifiedcontours[i].y - simplifiedcontours[i - 1].y) < minidelta_xy))
-    //         {
-    //             if (simplifiedcontours[i].y < center_point.y)
-    //             {
-    //                 if (simplifiedcontours[i].y < simplifiedcontours[i - 1].y)
-    //                 {
-    //                     simplifiedcontours[i - 1].x = simplifiedcontours[i].x;
-    //                     simplifiedcontours[i - 1].y = simplifiedcontours[i].y;
-    //                 }
-    //             }
-    //             else
-    //             {
-    //                 if (simplifiedcontours[i].y > simplifiedcontours[i - 1].y)
-    //                 {
-    //                     simplifiedcontours[i - 1].x = simplifiedcontours[i].x;
-    //                     simplifiedcontours[i - 1].y = simplifiedcontours[i].y;
-    //                 }
-    //             }
-    //             simplifiedcontours.erase(simplifiedcontours.begin() + i);
-    //         }
-    //     }
-    //     cout << "simplifiedcontours[" << i << "]: " << simplifiedcontours[i] << endl;
-    // }
     return simplifiedcontours;
 }
 
-float p2l_distance(Point2f point1, Point2f point2, Point2f point)
+Point findheighestPoint(vector<Point> contours)
 {
-    Point2f vec1 = point1 - point2;
-    Point2f vec2 = point - point2;
-    float cross = vec1.x * vec2.y - vec1.y * vec2.x;
-    float length = cv::norm(vec1);
-    float distance = cross / length;
-    return distance;
+    Point heighestPoint = contours[0];
+    for (int i = 0; i < contours.size(); i++)
+    {
+        if (contours[i].y < heighestPoint.y)
+        {
+            heighestPoint = contours[i];
+        }
+    }
+    return heighestPoint;
+}
+
+Point findlowestPoint(vector<Point> contours)
+{
+    Point lowestPoint = contours[0];
+    for (int i = 0; i < contours.size(); i++)
+    {
+        if (contours[i].y > lowestPoint.y)
+        {
+            lowestPoint = contours[i];
+        }
+    }
+    return lowestPoint;
+}
+
+float findaverheight(vector<Point> contours)
+{
+    float aver_height = 0;
+    for (int i = 0; i < contours.size(); i++)
+    {
+        aver_height += contours[i].y;
+    }
+    aver_height /= contours.size();
+    return aver_height;
+}
+
+void pairingContours(vector<Point> contours, vector<Point> &farthestpair, vector<Point> &closestpair, Mat &rawimg)
+{
+
+    farthestpair.clear();
+    closestpair.clear();
+    if (contours.size() < 4)
+    {
+        cout << "contours size is less than 4" << endl;
+        return;
+    }
+
+    Rect rect = boundingRect(contours);
+
+    vector<Point> upcontours;
+    vector<Point> downcontours;
+    float aver_up_y = 0;
+    float aver_down_y = 0;
+
+    for (int i = 0; i < contours.size(); i++)
+    {
+        if (contours[i].y < rect.y + rect.height / 2)
+        {
+            upcontours.push_back(contours[i]);
+        }
+        else if (contours[i].y > rect.y + rect.height / 2)
+        {
+            downcontours.push_back(contours[i]);
+        }
+    }
+    if (upcontours.size() == 0 || downcontours.size() == 0)
+    {
+        cout << "upcontours size or downcontours size is 0" << endl;
+        return;
+    }
+    aver_up_y = findaverheight(upcontours);
+    aver_down_y = findaverheight(downcontours);
+
+    for (int i = 0; i < downcontours.size(); i++)
+    {
+        if (fabsf(downcontours[i].y - aver_down_y) > 60)
+        {
+            downcontours.erase(downcontours.begin() + i);
+            i--;
+        }
+    }
+
+    for (int i = 0; i < upcontours.size(); i++)
+    {
+        if (fabsf(upcontours[i].y - aver_up_y) > 60)
+        {
+            upcontours.erase(upcontours.begin() + i);
+            i--;
+        }
+    }
+
+    if (upcontours.size() == 0 || downcontours.size() == 0)
+    {
+        cout << "upcontours size or downcontours size is 0" << endl;
+        return;
+    }
+
+    aver_down_y = findaverheight(downcontours);
+    aver_up_y = findaverheight(upcontours);
+
+    vector<KeyPoint> keypoints;
+    for (int i = 0; i < upcontours.size(); i++)
+    {
+        keypoints.push_back(KeyPoint(upcontours[i], 1));
+        drawKeypoints(rawimg, keypoints, rawimg, Scalar(0, 255, 0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        putText(rawimg, to_string(upcontours[i].x) + "," + to_string(upcontours[i].y) + "num: " + to_string(i), upcontours[i], FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 255, 0), 1);
+    }
+
+    for (int i = 0; i < downcontours.size(); i++)
+    {
+        keypoints.push_back(KeyPoint(downcontours[i], 1));
+        drawKeypoints(rawimg, keypoints, rawimg, Scalar(0, 255, 0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        putText(rawimg, to_string(downcontours[i].x) + "," + to_string(downcontours[i].y) + "num: " + to_string(i), downcontours[i], FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 255, 0), 1);
+    }
+
+    Point heightestupcontour = upcontours[0];
+    Point heightestdowncontour = downcontours[0];
+    Point lowestupcontour = upcontours[0];
+    Point lowestdowncontour = downcontours[0];
+    Point middleupcontour = upcontours[0];
+    Point middledowncontour = downcontours[0];
+
+    if (upcontours.size() != 0)
+    {
+        heightestupcontour = findheighestPoint(upcontours);
+        lowestupcontour = findlowestPoint(upcontours);
+    }
+    if (downcontours.size() != 0)
+    {
+        heightestdowncontour = findheighestPoint(downcontours);
+        lowestdowncontour = findlowestPoint(downcontours);
+    }
+
+    // cout << "heightestupcontour: " << heightestupcontour << endl;
+    // cout << "lowestdowncontour: " << lowestdowncontour << endl;
+    // cout << "lowestupcontour: " << lowestupcontour << endl;
+    // cout << "heightestdowncontour: " << heightestdowncontour << endl;
+
+    for (int i = 0; i < downcontours.size(); i++)
+    {
+
+        // cout << "heightestupcontour.x: " << heightestupcontour.x << endl;
+        if (abs(downcontours[i].x - heightestupcontour.x) < 20)
+        {
+            if (abs(downcontours[i].y - aver_down_y) < 10)
+            {
+                downcontours[i].x = heightestupcontour.x;
+                farthestpair.push_back(heightestupcontour);
+                farthestpair.push_back(downcontours[i]);
+                downcontours.erase(downcontours.begin() + i);
+                i--;
+            }
+        }
+
+        if (abs(downcontours[i].x - lowestupcontour.x) < 5)
+        {
+            if (abs(downcontours[i].y - aver_down_y) < 10)
+            {
+                closestpair.push_back(lowestupcontour);
+                closestpair.push_back(downcontours[i]);
+                downcontours.erase(downcontours.begin() + i);
+                i--;
+            }
+        }
+    }
+
+    // cout << "upcontours size is " << upcontours.size() << endl;
+    // cout << "downcontours size is " << downcontours.size() << endl;
+    // cout << "farthestpair is " << farthestpair << endl;
+    // cout << "closestpair is " << closestpair << endl;
+
+    if ((farthestpair.empty() || (closestpair.empty())))
+    {
+        for (int i = 0; i < upcontours.size(); i++)
+        {
+            for (int j = 0; j < downcontours.size(); j++)
+            {
+                if (abs(upcontours[i].x - downcontours[j].x) < 5)
+                {
+                    if ((abs(upcontours[i].y - aver_up_y) < 20)&&(abs(downcontours[j].y - aver_down_y) < 20))
+                    {
+                    if (farthestpair.empty())
+                    {
+                        farthestpair.push_back(upcontours[i]);
+                        farthestpair.push_back(downcontours[j]);
+                    }
+                    else if (closestpair.empty())
+                    {
+                        closestpair.push_back(upcontours[i]);
+                        closestpair.push_back(downcontours[j]);
+                    }
+                    }
+                }
+            }
+        }
+    }
 }
 
 float cubemeasure(Mat rawimg, Mat depimg)
 {
-    vector<KeyPoint> keypoints;
-    rawimg = imread(testimg_path);
+
     // rawimg.resize(640, 480);
     // imshow("rawimg", rawimg);
     // waitKey(1);
     Mat threshimg;
-    predispose(rawimg, threshimg, 5, 36, 28);
+    predispose(rawimg, threshimg, 3, 35, 28);
 
     vector<vector<Point>> contours;
     vector<Vec4i> hierarchy;
     findContours(threshimg, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_NONE);
 
-    vector<Point> target_contours;
-    target_contours = contoursfilter(contours);
-    for (int i = 0; i < target_contours.size(); i++)
+    if (contours.empty())
     {
-        keypoints.push_back(KeyPoint(target_contours[i], 1));
-        drawKeypoints(rawimg, keypoints, rawimg, Scalar(0, 255, 0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-        putText(rawimg, to_string(target_contours[i].x) + "," + to_string(target_contours[i].y) + "num: " + to_string(i), target_contours[i], FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 255, 0), 1);
+        cout << "contours empty" << endl;
+        return 0;
     }
 
-    Rect rect = boundingRect(target_contours);
-    int originx = rect.x;
-    int originy = rect.y;
-    int originwidth = rect.width;
-    int originheight = rect.height;
-    int shrinkwidth = originwidth * 0.2;
-    int shrinkheight = originheight * 1.2;
-    Rect shrinkrect(rect.x + (originwidth - shrinkwidth) / 2, rect.y + (originheight - shrinkheight) / 2, shrinkwidth, shrinkheight);
-
-    Mat Mask = Mat::zeros(rawimg.size(), CV_8UC1);
-    rectangle(Mask, shrinkrect, Scalar(255), -1);
-    bitwise_and(threshimg, Mask, threshimg);
-
-    findContours(threshimg, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_NONE);
-    // cout << "contours.size(): " << contours.size() << endl;
-    target_contours = contoursfilter(contours);
-    vector<Point> upcontours;
-    vector<Point> downcontours;
-    for (int i = 0; i < target_contours.size(); i++)
+    else
     {
-        if (target_contours[i].y < shrinkrect.y + shrinkrect.height / 2 && upcontours.size() < 2)
+        vector<Point> maxsizeContours;
+        findmaxsizeContours(contours, maxsizeContours);
+        if (maxsizeContours.size() < 500)
         {
-            upcontours.push_back(target_contours[i]);
+            cout << "contours too small" << endl;
+            return 0;
         }
-        else if (target_contours[i].y > shrinkrect.y + shrinkrect.height / 2 && downcontours.size() < 2)
+
+        vector<Point> target_contours;
+        target_contours = contoursfilter(contours);
+
+        vector<Point> farthestpair;
+        vector<Point> closestpair;
+        pairingContours(target_contours, farthestpair, closestpair, rawimg);
+        if (!farthestpair.empty())
         {
-            downcontours.push_back(target_contours[i]);
+            line(rawimg, farthestpair[0], farthestpair[1], Scalar(255, 255, 255), 2);
         }
+        if (!closestpair.empty())
+        {
+            line(rawimg, closestpair[0], closestpair[1], Scalar(255, 255, 255), 2);
+        }
+        if (farthestpair.empty() && closestpair.empty())
+        {
+            cout << "no pair" << endl;
+            return 0;
+        }
+
+        float dist = 10;
+        cout << "dist: " << fabsf(dist) << endl;
+        return fabsf(dist);
     }
-    line(rawimg, upcontours[0], upcontours[upcontours.size() - 1], Scalar(255, 255, 255), 1);
-    line(rawimg, downcontours[0], downcontours[downcontours.size() - 1], Scalar(255, 255, 255), 1);
-
-    float dist = (p2l_distance(upcontours[0], upcontours[upcontours.size() - 1], downcontours[0]) + p2l_distance(upcontours[0], upcontours[upcontours.size() - 1], downcontours[downcontours.size() - 1])) / 2;
-    cout << "dist: " << fabsf(dist) << endl;
-
-    // rectangle(rawimg, srinkrect, Scalar(0, 255, 0), 2);
-    // imshow("rawimg", rawimg);
-    // waitKey(1);
-
-    // for (int i = 0; i < target_contours.size(); i++)
-    // {
-    //     keypoints.push_back(KeyPoint(target_contours[i], 1));
-    //     drawKeypoints(rawimg, keypoints, rawimg, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    //     putText(rawimg, to_string(target_contours[i].x) + "," + to_string(target_contours[i].y) + "num: " + to_string(i), target_contours[i], FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 1);
-    // }
-    // drawContours(rawimg, vector<vector<Point>>{target_contours}, -1, Scalar(255, 255, 255), 1);
-
-    //
-
-    // float theta[target_contours.size()] = {0};
-    // for (int i = 0; i < target_contours.size(); i++)
-    // {
-    //     if (i == 0)
-    //     {
-    //         theta[0] = atan2(target_contours[i].y - target_contours[target_contours.size() - 1].y, target_contours[i].x - target_contours[target_contours.size() - 1].x);
-    //     }
-    //     else
-    //     {
-    //         theta[i] = atan2(target_contours[i].y - target_contours[i - 1].y, target_contours[i].x - target_contours[i - 1].x);
-    //     }
-    //     cout << "theta[" << i << "]: " << theta[i] << endl;
-    // }
-    // float theta_dv = 5.f;
-
-    // for (int i = 0; i < target_contours.size(); i++)
-    // {
-    //     keypoints.push_back(KeyPoint(target_contours[i], 1));
-    //     drawKeypoints(rawimg, keypoints, rawimg, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    //     putText(rawimg, to_string(target_contours[i].x) + "," + to_string(target_contours[i].y) + "num: " + to_string(i), target_contours[i], FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 1);
-    // }
-    // drawContours(rawimg, vector<vector<Point>>{target_contours}, -1, Scalar(255, 255, 255), 1);
-
-    imshow("rawimg", rawimg);
-    waitKey(0);
-
-    return 0;
 }
 
 // int main(int argc, char **argv)
@@ -272,17 +369,23 @@ int main(int argc, char **argv)
     ros::Subscriber sub_depth = nh.subscribe("/camera2/depth/image_raw", 1, depthCallback);
     ros::Publisher pub = nh.advertise<std_msgs::String>("cubemeasure_result_str", 1);
     ros::Rate loop_rate(10);
+    namedWindow("cubemeasure", WINDOW_AUTOSIZE);
+    startWindowThread();
     while (ros::ok())
     {
         ros::spinOnce();
-        // float length = cubemeasure(raw_img, raw_depth);
-        // imshow("raw_img", raw_img);
-        // imshow("raw_depth", raw_depth);
-        std_msgs::String msg;
-        std::stringstream ss;
-        // ss << length;
-        msg.data = ss.str();
-        pub.publish(msg);
+        if (raw_img.empty() || raw_depth.empty())
+        {
+            continue;
+        }
+        float length = cubemeasure(raw_img, raw_depth);
+        // cout << "length: " << length << endl;
+        imshow("cubemeasure", raw_img);
+        // std_msgs::String msg;
+        // std::stringstream ss;
+        // ss << "length";
+        // msg.data = ss.str();
+        // pub.publish(msg);
         loop_rate.sleep();
     }
 
